f017e367c2aa30d9409feb87c12d1c8da809db8f
samlifei <samlfei@163.com>
2019-05-29 15:41:20 +0800

    添加工程文件

---
+++ SWBaseKit/SWBaseKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWBaseKit
+//
+//  Created by ttxc on 2019/5/29.
+//  Copyright © 2019 http://www.jianshu.com/u/84d0142cbec9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SWBaseKit/SWBaseKit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SWBaseKit
+//
+//  Created by ttxc on 2019/5/29.
+//  Copyright © 2019 http://www.jianshu.com/u/84d0142cbec9. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
