076087b63773ef92724ff086bfab6b5a2a6827c3
samlifei <samlfei@163.com>
2019-06-10 08:55:43 +0800

    网络判断

--- SWBaseKit/SWBaseKit/Global/NetWork/HttpManager.m
+++ SWBaseKit/SWBaseKit/Global/NetWork/HttpManager.m
@@ -30,24 +30,39 @@
 {
     [self networkNotOK];
     AFHTTPSessionManager *manager = [self httpManager];
-    [manager POST:urlStr
-       parameters:parameters
-         progress:^(NSProgress * _Nonnull uploadProgress) {
-             
-         }
-          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-              
-              NSMutableDictionary *resDict = (NSMutableDictionary *)[responseObject mj_JSONObject];
-//              NSDictionary *dict = [HttpManager checkResultVaild:resDict withFunction:parameters[@"function"]];
-              
-              SWLog(@"\n接口：%@\n返回数据：%@",parameters[@"function"],[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
-              
-              success(resDict);
-          }
-          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-              failure(error);
-              SWLog(@"\n当前接口：%@\n错误信息：%@",parameters[@"function"],error);
-          }];
+    
+    [manager POST:urlStr parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSMutableDictionary *resDict = (NSMutableDictionary *)[responseObject mj_JSONObject];
+//        NSDictionary *dict = [HttpManager checkResultVaild:resDict withFunction:parameters[@"function"]];
+        SWLog(@"\n接口：%@\n返回数据：%@",parameters[@"function"],[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+        success(resDict);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        failure(error);
+        SWLog(@"\n当前接口：%@\n错误信息：%@",parameters[@"function"],error);
+    }];
+    
+    
+    
+//    [manager POST:urlStr
+//       parameters:parameters
+//         progress:^(NSProgress * _Nonnull uploadProgress) {
+//
+//         }
+//          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//
+//              NSMutableDictionary *resDict = (NSMutableDictionary *)[responseObject mj_JSONObject];
+////              NSDictionary *dict = [HttpManager checkResultVaild:resDict withFunction:parameters[@"function"]];
+//              SWLog(@"\n接口：%@\n返回数据：%@",parameters[@"function"],[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+//              success(resDict);
+//          }
+//          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//              failure(error);
+//              SWLog(@"\n当前接口：%@\n错误信息：%@",parameters[@"function"],error);
+//          }];
 }
 
 + (void)getUrl:(NSString *)urlStr
@@ -94,21 +109,47 @@
  *
  **/
 +(void)networkNotOK {
-    if ([[AFNetworkReachabilityManager sharedManager] networkReachabilityStatus] <= 0) {
-        
-        // Set the label text.
-        self.hud.label.text = NSLocalizedString(@"网络无连接", @"HUD loading title");
-        // You can also adjust other label properties if needed.
-        // hud.label.font = [UIFont italicSystemFontOfSize:16.f];
-        
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
-            sleep(3.);
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self->hud hideAnimated:YES];
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        int netWork;
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:{
+                NSLog(@"未知");
+                netWork = -1;
+            }
+                break;
+            case AFNetworkReachabilityStatusNotReachable:{
+                NSLog(@"没有网络");
+                netWork = 0;
+            }
+                break;
+            case AFNetworkReachabilityStatusReachableViaWWAN:{
+                NSLog(@"3G/4G");
+                netWork = 1;
+            }
+                break;
+            case AFNetworkReachabilityStatusReachableViaWiFi: {
+                NSLog(@"无线网");
+                netWork = 2;
+            }
+                break;
+            default:
+                NSLog(@"网络?");
+                break;
+        }
+        if (netWork <= 0) {
+            MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:((AppDelegate*)([UIApplication sharedApplication].delegate)).window animated:YES];
+            hud.label.text = NSLocalizedString(@"网络无连接", @"HUD loading title");
+            // hud.label.font = [UIFont italicSystemFontOfSize:16.f];
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
+                sleep(3.);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [hud hideAnimated:YES];
+                });
             });
-        });
+        }
         return;
-    }
+    }];
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
 }
 -(MBProgressHUD *)hud {
     if (_hud ==nil) {
